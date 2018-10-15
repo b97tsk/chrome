19ce4bfdd02e53f94bf77c2b467da4c95c451a72
b97tsk <b97tsk@gmail.com>
2018-10-15 23:23:48 +0800

    Add vmess.go

---
+++ vmess.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"log"
+	"net"
+	"net/url"
+	"strings"
+
+	"github.com/gogo/protobuf/proto"
+	"gopkg.in/yaml.v2"
+	"v2ray.com/core"
+	_ "v2ray.com/core/main/distro/all"
+	"v2ray.com/ext/tools/conf"
+)
+
+type vmessOptions struct {
+	URL string
+}
+
+type vmessService struct{}
+
+func (vmessService) Run(ctx ServiceCtx) {
+	localAddr, localPort, err := net.SplitHostPort(ctx.ListenAddr)
+	if err != nil {
+		log.Printf("[vmess] %v\n", err)
+		return
+	}
+
+	var instance *core.Instance
+	defer func() {
+		if instance != nil {
+			err := instance.Close()
+			if err != nil {
+				log.Printf("[vmess] close instance: %v\n", err)
+			}
+			instance = nil
+			log.Printf("[vmess] stopped listening on %v\n", ctx.ListenAddr)
+		}
+	}()
+
+	var (
+		options vmessOptions
+	)
+	for {
+		select {
+		case data := <-ctx.Events:
+			if new, ok := data.(vmessOptions); ok {
+				old := options
+				options = new
+				if new.URL != old.URL {
+					if instance != nil {
+						err := instance.Close()
+						if err != nil {
+							log.Printf("[vmess] close instance: %v\n", err)
+						}
+						instance = nil
+						log.Printf("[vmess] stopped listening on %v\n", ctx.ListenAddr)
+					}
+					instance, err = vmessParseURL(new.URL, localAddr, localPort)
+					if err != nil {
+						log.Printf("[vmess] parse url: %v\n", err)
+						break
+					}
+					err = instance.Start()
+					if err != nil {
+						log.Printf("[vmess] start instance: %v\n", err)
+						break
+					}
+					log.Printf("[vmess] listening on %v\n", ctx.ListenAddr)
+				}
+			}
+		case <-ctx.Done:
+			return
+		}
+	}
+}
+
+func (vmessService) UnmarshalOptions(text []byte) (interface{}, error) {
+	var options vmessOptions
+	if err := yaml.UnmarshalStrict(text, &options.URL); err != nil {
+		return nil, err
+	}
+	return options, nil
+}
+
+func init() {
+	services.Add("vmess", vmessService{})
+}
+
+func vmessParseURL(rawurl, localAddr, localPort string) (*core.Instance, error) {
+	u, err := url.Parse(rawurl)
+	if err != nil || u.Scheme != "vmess" {
+		return nil, errors.New("invalid vmess: " + rawurl)
+	}
+	data, err := decodeBase64String(u.Host)
+	if err != nil {
+		return nil, errors.New("invalid vmess: " + rawurl)
+	}
+
+	var c struct {
+		Version string `json:"v"`
+
+		Net  string `json:"net"`
+		TLS  string `json:"tls"`
+		Type string `json:"type"`
+
+		Address string `json:"add"`
+		Port    string `json:"port"`
+		ID      string `json:"id"`
+		AlterID string `json:"aid"`
+
+		Path string `json:"path"`
+		Host string `json:"host"`
+
+		LocalAddr string `json:"-"`
+		LocalPort string `json:"-"`
+	}
+
+	if err := json.Unmarshal(data, &c); err != nil {
+		return nil, errors.New("invalid vmess: " + rawurl)
+	}
+
+	var templateName string
+
+	switch c.Net + "/" + c.TLS {
+	case "kcp/", "kcp/none":
+		if c.Type == "" {
+			c.Type = "none"
+		}
+		templateName = "kcp"
+	case "tcp/", "tcp/none":
+		templateName = "tcp"
+		if c.Type == "http" {
+			if c.Version == "" {
+				fields := strings.SplitN(c.Path, ";", 2)
+				if len(fields) == 2 {
+					c.Path, c.Host = fields[0], fields[1]
+				}
+			}
+			if c.Path == "" {
+				c.Path = "/"
+			}
+			if c.Host == "" {
+				c.Host = c.Address
+			}
+			templateName = "tcp/http"
+		}
+	case "tcp/tls":
+		if c.Host == "" {
+			c.Host = c.Address
+		}
+		templateName = "tcp/tls"
+	case "ws/", "ws/none":
+		if c.Version == "" && c.Path == "" {
+			c.Path = c.Host
+		}
+		templateName = "ws"
+	case "h2/tls", "ws/tls":
+		if c.Version == "" {
+			fields := strings.SplitN(c.Path, ";", 2)
+			if len(fields) == 2 {
+				c.Path, c.Host = fields[0], fields[1]
+			}
+		}
+		if c.Path == "" {
+			c.Path = "/"
+		}
+		if c.Host == "" {
+			c.Host = c.Address
+		}
+		templateName = c.Net + "/" + c.TLS
+	}
+
+	tpl := vmessTemplate.Lookup(templateName)
+	if tpl == nil {
+		return nil, errors.New("unknown vmess: " + rawurl)
+	}
+
+	c.LocalAddr, c.LocalPort = localAddr, localPort
+
+	buf := new(bytes.Buffer)
+
+	if err := tpl.Execute(buf, &c); err != nil {
+		return nil, err
+	}
+
+	config := conf.Config{}
+
+	if err := json.Unmarshal(buf.Bytes(), &config); err != nil {
+		return nil, err
+	}
+
+	pb, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	pbBytes, err := proto.Marshal(pb)
+	if err != nil {
+		return nil, err
+	}
+
+	pbBuffer := bytes.NewBuffer(pbBytes)
+	coreConfig, err := core.LoadConfig("protobuf", ".pb", pbBuffer)
+	if err != nil {
+		return nil, err
+	}
+
+	return core.New(coreConfig)
+}
---
+++ vmessTemplate.go
@@ -0,0 +1,326 @@
+package main
+
+import (
+	"text/template"
+)
+
+var vmessTemplate *template.Template
+
+func init() {
+	vmessTemplate = template.New("vmess")
+	template.Must(vmessTemplate.New("h2/tls").Parse(vmess_h2_tls_jsonString))
+	template.Must(vmessTemplate.New("kcp").Parse(vmess_kcp_jsonString))
+	template.Must(vmessTemplate.New("tcp").Parse(vmess_tcp_jsonString))
+	template.Must(vmessTemplate.New("tcp/http").Parse(vmess_tcp_http_jsonString))
+	template.Must(vmessTemplate.New("tcp/tls").Parse(vmess_tcp_tls_jsonString))
+	template.Must(vmessTemplate.New("ws").Parse(vmess_ws_jsonString))
+	template.Must(vmessTemplate.New("ws/tls").Parse(vmess_ws_tls_jsonString))
+}
+
+var vmess_h2_tls_jsonString = `
+{
+  "inbound": {
+    "port": {{.LocalPort}},
+    "listen": "{{.LocalAddr}}",
+    "protocol": "socks",
+    "settings": {}
+  },
+  "outbound": {
+    "protocol": "vmess",
+    "settings": {
+      "vnext": [
+        {
+          "address": "{{.Address}}",
+          "port": {{.Port}},
+          "users": [
+            {
+              "id": "{{.ID}}",
+              "alterId": {{.AlterID}},
+              "security": "auto",
+              "level": 0
+            }
+          ]
+        }
+      ]
+    },
+    "streamSettings": {
+      "network": "h2",
+      "security": "tls",
+      "h2Settings": {
+        "host": ["{{.Host}}"],
+        "path": "{{.Path}}"
+      }
+    }
+  },
+  "policy": {
+    "levels": {
+      "0": {"uplinkOnly": 0}
+    }
+  }
+}
+`
+
+var vmess_kcp_jsonString = `
+{
+  "inbound": {
+    "port": {{.LocalPort}},
+    "listen": "{{.LocalAddr}}",
+    "protocol": "socks",
+    "settings": {}
+  },
+  "outbound": {
+    "protocol": "vmess",
+    "settings": {
+      "vnext": [
+        {
+          "address": "{{.Address}}",
+          "port": {{.Port}},
+          "users": [
+            {
+              "id": "{{.ID}}",
+              "alterId": {{.AlterID}},
+              "security": "auto",
+              "level": 0
+            }
+          ]
+        }
+      ]
+    },
+    "streamSettings": {
+      "network": "kcp",
+      "kcpSettings": {
+        "mtu": 1350,
+        "tti": 50,
+        "uplinkCapacity": 2,
+        "downlinkCapacity": 100,
+        "congestion": false,
+        "readBufferSize": 2,
+        "writeBufferSize": 2,
+        "header": {"type": "{{.Type}}"}
+      }
+    }
+  },
+  "policy": {
+    "levels": {
+      "0": {"uplinkOnly": 0}
+    }
+  }
+}
+`
+
+var vmess_tcp_jsonString = `
+{
+  "inbound": {
+    "port": {{.LocalPort}},
+    "listen": "{{.LocalAddr}}",
+    "protocol": "socks",
+    "settings": {}
+  },
+  "outbound": {
+    "protocol": "vmess",
+    "settings": {
+      "vnext": [
+        {
+          "address": "{{.Address}}",
+          "port": {{.Port}},
+          "users": [
+            {
+              "id": "{{.ID}}",
+              "alterId": {{.AlterID}},
+              "security": "auto",
+              "level": 0
+            }
+          ]
+        }
+      ]
+    },
+    "streamSettings": {
+      "network": "tcp"
+    }
+  },
+  "policy": {
+    "levels": {
+      "0": {"uplinkOnly": 0}
+    }
+  }
+}
+`
+
+var vmess_tcp_http_jsonString = `
+{
+  "inbound": {
+    "port": {{.LocalPort}},
+    "listen": "{{.LocalAddr}}",
+    "protocol": "socks",
+    "settings": {}
+  },
+  "outbound": {
+    "protocol": "vmess",
+    "settings": {
+      "vnext": [
+        {
+          "address": "{{.Address}}",
+          "port": {{.Port}},
+          "users": [
+            {
+              "id": "{{.ID}}",
+              "alterId": {{.AlterID}},
+              "security": "auto",
+              "level": 0
+            }
+          ]
+        }
+      ]
+    },
+    "streamSettings": {
+      "network": "tcp",
+      "tcpSettings": {
+        "header": {
+          "type": "http",
+          "request": {
+            "version": "1.1",
+            "method": "GET",
+            "path": ["{{.Path}}"],
+            "headers": {
+              "Host": ["{{.Host}}"]
+            }
+          }
+        }
+      }
+    }
+  },
+  "policy": {
+    "levels": {
+      "0": {"uplinkOnly": 0}
+    }
+  }
+}
+`
+
+var vmess_tcp_tls_jsonString = `
+{
+  "inbound": {
+    "port": {{.LocalPort}},
+    "listen": "{{.LocalAddr}}",
+    "protocol": "socks",
+    "settings": {}
+  },
+  "outbound": {
+    "protocol": "vmess",
+    "settings": {
+      "vnext": [
+        {
+          "address": "{{.Address}}",
+          "port": {{.Port}},
+          "users": [
+            {
+              "id": "{{.ID}}",
+              "alterId": {{.AlterID}},
+              "security": "auto",
+              "level": 0
+            }
+          ]
+        }
+      ]
+    },
+    "streamSettings": {
+      "network": "tcp",
+      "security": "tls",
+      "tlsSettings": {
+        "serverName": "{{.Host}}",
+        "allowInsecure": false
+      }
+    }
+  },
+  "policy": {
+    "levels": {
+      "0": {"uplinkOnly": 0}
+    }
+  }
+}
+`
+
+var vmess_ws_jsonString = `
+{
+  "inbound": {
+    "port": {{.LocalPort}},
+    "listen": "{{.LocalAddr}}",
+    "protocol": "socks",
+    "settings": {}
+  },
+  "outbound": {
+    "protocol": "vmess",
+    "settings": {
+      "vnext": [
+        {
+          "address": "{{.Address}}",
+          "port": {{.Port}},
+          "users": [
+            {
+              "id": "{{.ID}}",
+              "alterId": {{.AlterID}},
+              "security": "auto",
+              "level": 0
+            }
+          ]
+        }
+      ]
+    },
+    "streamSettings": {
+      "network": "ws",
+      "wsSettings": {
+        "path": "{{.Path}}",
+        "headers": {}
+      }
+    }
+  },
+  "policy": {
+    "levels": {
+      "0": {"uplinkOnly": 0}
+    }
+  }
+}
+`
+
+var vmess_ws_tls_jsonString = `
+{
+  "inbound": {
+    "port": {{.LocalPort}},
+    "listen": "{{.LocalAddr}}",
+    "protocol": "socks",
+    "settings": {}
+  },
+  "outbound": {
+    "protocol": "vmess",
+    "settings": {
+      "vnext": [
+        {
+          "address": "{{.Address}}",
+          "port": {{.Port}},
+          "users": [
+            {
+              "id": "{{.ID}}",
+              "alterId": {{.AlterID}},
+              "security": "auto",
+              "level": 0
+            }
+          ]
+        }
+      ]
+    },
+    "streamSettings": {
+      "network": "ws",
+      "security": "tls",
+      "wsSettings": {
+        "path": "{{.Path}}",
+        "headers": {"Host": "{{.Host}}"}
+      }
+    }
+  },
+  "policy": {
+    "levels": {
+      "0": {"uplinkOnly": 0}
+    }
+  }
+}
+`
