2bd8973efae014467bec81691291602eb1670bdd
b97tsk <b97tsk@gmail.com>
2019-07-16 12:21:26 +0800

    Update service/shadowsocks/shadowsocks.go

--- service/shadowsocks/shadowsocks.go
+++ service/shadowsocks/shadowsocks.go
@@ -3,7 +3,7 @@ package shadowsocks
 import (
 	"log"
 	"net"
-	"sync/atomic"
+	"strings"
 	"time"
 
 	"github.com/b97tsk/chrome/configure"
@@ -15,6 +15,7 @@ import (
 )
 
 type Options struct {
+	Server   string
 	Method   string
 	Password string
 	Proxy    proxy.ProxyChain `yaml:"over"`
@@ -36,36 +37,76 @@ func (Service) Run(ctx service.Context) {
 	defer log.Printf("[shadowsocks] stopped listening on %v\n", ln.Addr())
 	defer ln.Close()
 
-	type Cipher struct {
-		core.Cipher
+	type X struct {
+		Options
+		Dialer proxy.Dialer
+		Cipher core.Cipher
 	}
-
-	var (
-		dial   atomic.Value
-		cipher atomic.Value
-	)
-	dial.Store(direct.Dial)
+	xin, xout := make(chan X), make(chan X)
+	defer func() {
+		close(xin)
+		for range xout {
+			// Let's wait until xout closes.
+			// Though this is not necessary.
+		}
+	}()
+	go func(x X) {
+		ok := true
+		for ok {
+			select {
+			case x, ok = <-xin:
+			case xout <- x:
+			}
+		}
+		close(xout)
+	}(X{Dialer: direct})
 
 	ctx.Manager.ServeListener(ln, func(c net.Conn) {
-		cipherLoad := cipher.Load
-		cipher := cipherLoad()
-		if cipher == nil {
+		x, ok := <-xout
+		if !ok {
+			return
+		}
+		if x.Cipher == nil {
 			time.Sleep(time.Second)
-			cipher = cipherLoad()
-			if cipher == nil {
+			x = <-xout
+			if x.Cipher == nil {
 				return
 			}
 		}
 
-		c = cipher.(Cipher).StreamConn(c)
+		if x.Server != "" { // Client mode.
+			addr, err := socks.Handshake(c)
+			if err != nil {
+				log.Printf("[shadowsocks] socks handshake: %v\n", err)
+				return
+			}
+
+			rc, err := x.Dialer.Dial("tcp", x.Server)
+			if err != nil {
+				// log.Printf("[shadowsocks] %v\n", err)
+				return
+			}
+			defer rc.Close()
+
+			rc = x.Cipher.StreamConn(rc)
+			_, err = rc.Write(addr)
+			if err != nil {
+				// log.Printf("[shadowsocks] %v\n", err)
+				return
+			}
+
+			utility.Relay(rc, c)
+			return
+		}
+
+		c = x.Cipher.StreamConn(c)
 		addr, err := socks.ReadAddr(c)
 		if err != nil {
 			log.Printf("[shadowsocks] read addr: %v\n", err)
 			return
 		}
 
-		dial := dial.Load().(func(network, addr string) (net.Conn, error))
-		rc, err := dial("tcp", addr.String())
+		rc, err := x.Dialer.Dial("tcp", addr.String())
 		if err != nil {
 			// log.Printf("[shadowsocks] %v\n", err)
 			return
@@ -75,26 +116,37 @@ func (Service) Run(ctx service.Context) {
 		utility.Relay(rc, c)
 	})
 
-	var (
-		options Options
-	)
+	var options Options
 	for {
 		select {
 		case data := <-ctx.Events:
 			if new, ok := data.(Options); ok {
 				old := options
 				options = new
+				x := <-xout
+				x.Options = new
 				if new.Method != old.Method || new.Password != old.Password {
-					if c, err := core.PickCipher(new.Method, nil, new.Password); err != nil {
-						log.Printf("[shadowsocks] pick cipher: %v\n", err)
-					} else {
-						cipher.Store(Cipher{c})
+					password := new.Password
+					if strings.HasPrefix(password, "base64:") {
+						bytes, err := utility.DecodeBase64String(password[7:])
+						if err != nil {
+							log.Println("[shadowsocks] fatal: password is not a valid base64 string")
+							return
+						}
+						password = string(bytes)
 					}
+					cipher, err := core.PickCipher(new.Method, nil, password)
+					if err != nil {
+						log.Printf("[shadowsocks] fatal: pick cipher: %v\n", err)
+						return
+					}
+					x.Cipher = cipher
 				}
 				if !new.Proxy.Equals(old.Proxy) {
 					d, _ := new.Proxy.NewDialer(direct)
-					dial.Store(d.Dial)
+					x.Dialer = d
 				}
+				xin <- x
 			}
 		case <-ctx.Done:
 			return
