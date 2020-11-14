200fef6d5408b75657e4f93f5dfdd47e175945f9
b97tsk <b97tsk@gmail.com>
2020-11-14 15:05:18 +0800

    Update service/relay.go

--- service/relay.go
+++ service/relay.go
@@ -3,39 +3,32 @@ package service
 import (
 	"io"
 	"net"
-	"os"
 	"sync"
 	"time"
 )
 
-func Relay(left, right net.Conn) error {
-	c := make(chan error, 2)
+func Relay(c1, c2 net.Conn) {
+	var wg sync.WaitGroup
+	wg.Add(2)
 	go func() {
-		buf := relayPool.Get()
-		defer relayPool.Put(buf)
-		_, err := io.CopyBuffer(left, right, (*buf.(*relayBuffer))[:])
-		left.SetReadDeadline(time.Now())
-		c <- err
+		defer wg.Done()
+		b := relayPool.Get().(*relayBuffer)
+		defer relayPool.Put(b)
+		io.CopyBuffer(c1, c2, (*b)[:])
+		c1.SetReadDeadline(time.Now())
 	}()
 	go func() {
-		buf := relayPool.Get()
-		defer relayPool.Put(buf)
-		_, err := io.CopyBuffer(right, left, (*buf.(*relayBuffer))[:])
-		right.SetReadDeadline(time.Now())
-		c <- err
+		defer wg.Done()
+		b := relayPool.Get().(*relayBuffer)
+		defer relayPool.Put(b)
+		io.CopyBuffer(c2, c1, (*b)[:])
+		c2.SetReadDeadline(time.Now())
 	}()
-	e1, e2 := <-c, <-c
-	if e1 != nil && !os.IsTimeout(e1) {
-		return e1
-	}
-	if e2 != nil && !os.IsTimeout(e2) {
-		return e2
-	}
-	return nil
+	wg.Wait()
 }
 
 type relayBuffer [32 * 1024]byte
 
 var relayPool = sync.Pool{
-	New: func() interface{} { return &relayBuffer{} },
+	New: func() interface{} { return new(relayBuffer) },
 }
